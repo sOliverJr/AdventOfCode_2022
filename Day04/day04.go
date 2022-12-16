794a9a01d6c1382fd9dc5bac48aa342f19fab50e
Oliver Seider <64847610+sOliverJr@users.noreply.github.com>
2022-12-16 16:22:54 +0100

    Day04 Part2

--- Day04/day04.go
+++ Day04/day04.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 )
 
-func ReturnInputFromFile(path string) []string{
+func ReturnInputFromFile(path string) []string {
 	var InputArray []string
 	f, _ := os.Open(path)
 
@@ -42,13 +42,30 @@ func TestIfOneRangeContainsTheOther(SingleElfPair []string) bool {
 	RangeTwoLower, _ := strconv.Atoi(RangeTwo[0])
 	RangeTwoHigher, _ := strconv.Atoi(RangeTwo[1])
 
-	if RangeOneLower <= RangeTwoLower && RangeOneHigher >= RangeTwoHigher{
+	if RangeOneLower <= RangeTwoLower && RangeOneHigher >= RangeTwoHigher {
 		// fmt.Printf("Range One Contains Range Two:   Range 1: %v --- Range 2: %v \n", RangeOne, RangeTwo)
 		return true
-	}else if RangeTwoLower <= RangeOneLower && RangeTwoHigher >= RangeOneHigher{
+	} else if RangeTwoLower <= RangeOneLower && RangeTwoHigher >= RangeOneHigher {
 		// fmt.Printf("Range Two Contains Range One:   Range 1: %v -- Range 2: %v \n", RangeOne, RangeTwo)
 		return true
-	}else {
+	} else {
+		return false
+	}
+}
+
+func TestIfRangesOverlapAtAll(SingleElfPair []string) bool {
+	var RangeOne []string = strings.Split(SingleElfPair[0], "-")
+	var RangeTwo []string = strings.Split(SingleElfPair[1], "-")
+
+	RangeOneLower, _ := strconv.Atoi(RangeOne[0])
+	RangeOneHigher, _ := strconv.Atoi(RangeOne[1])
+	RangeTwoLower, _ := strconv.Atoi(RangeTwo[0])
+	RangeTwoHigher, _ := strconv.Atoi(RangeTwo[1])
+
+	if RangeOneHigher >= RangeTwoLower && RangeTwoHigher >= RangeOneLower {
+		fmt.Printf("Ranges overlap:   Range 1: %v --- Range 2: %v \n", RangeOne, RangeTwo)
+		return true
+	} else {
 		return false
 	}
 }
@@ -56,13 +73,18 @@ func TestIfOneRangeContainsTheOther(SingleElfPair []string) bool {
 func main() {
 	var FileInput []string = ReturnInputFromFile("day04_input.txt")
 	var FormattedFileInput [][]string = FormatFileInput(FileInput)
-	var AmountElfPairs int = 0
+	var AmountElfPairsContain int = 0
+	var AmountElfPairsOverlap int = 0
 
-	for _, SingleElfPair := range FormattedFileInput{
-		if TestIfOneRangeContainsTheOther(SingleElfPair){
-			AmountElfPairs += 1
+	for _, SingleElfPair := range FormattedFileInput {
+		if TestIfOneRangeContainsTheOther(SingleElfPair) {
+			AmountElfPairsContain += 1
+		}
+		if TestIfRangesOverlapAtAll(SingleElfPair) {
+			AmountElfPairsOverlap += 1
 		}
 	}
 
-	fmt.Printf("The amount of elf pairs: %v", AmountElfPairs)
+	fmt.Printf("Part 1: %v \n", AmountElfPairsContain)
+	fmt.Printf("Part 2: %v \n", AmountElfPairsOverlap)
 }
